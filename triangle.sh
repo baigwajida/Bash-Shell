6122f299a99943cd87ca8752b2931d6899dfcc78
baigwajida <37880067+baigwajida@users.noreply.github.com>
2018-07-17 11:08:52 +0530

    Create triangle.sh

---
+++ triangle.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+# Check whether triangle is Scalene, Isosceles, or Equilateral.
+read a b c
+if [[ ($a -eq $b) && ($b -eq $c) ]]
+then 
+    echo "EQUILATERAL"
+elif [[ ($a -ne $b) && ($b -ne $c) && ($c -ne $a) ]]
+then
+    echo "SCALENE"
+else
+    echo "ISOSCELES"
+fi
